6ca1226b7e1fc256cbfb8222215b413c4f430103
Cleyton de Castro <cleytoncm@gmail.com>
2024-05-09 18:00:50 -0300

    atualização do arquivo de falta de boas práticas

--- sem-boas-praticas/update_data.php
+++ sem-boas-praticas/update_data.php
@@ -5,17 +5,37 @@ require_once 'autoload.php';
 use Model\Pessoa;
 
 if($_SERVER["REQUEST_METHOD"]=="POST"){
+$pessoa=new Pessoa();
+if ($_POST['nome']){
+$nome=$_POST['nome'];
+if ($_POST['idade']){
+$idade=$_POST['idade'];
+if ($_POST['endereco']){
+$cidade=$_POST['cidade'];
+if ($_POST['telefone']){
+$telefone=$_POST['telefone'];
+} else {
+echo 'Campo inválido';
+}
+} else {
+echo 'Campo inválido';
+}
+} else {
+echo 'Campo inválido';
+}
+} else {
+echo 'Campo inválido';
+}
 $nome=$_POST['nome'];
 $idade=$_POST['idade'];
 $cidade=$_POST['cidade'];
 $endereco=$_POST['endereco'];
 $telefone=$_POST['telefone'];
-
-
-$pessoa=new Pessoa();
-
-$pessoa->atualizarDados($nome,$idade,$cidade,$endereco,$telefone);
-
+//$pessoa->nome=$_POST['nome'];
+//$pessoa->idade=$_POST['idade'];
+//$pessoa->cidade=$_POST['cidade'];
+//$pessoa->endereco=$_POST['endereco'];
+//$pessoa->telefone=$_POST['telefone'];
 if($pessoa->idade>=0&&$pessoa->idade<10){
 echo"Criança";
 }else{
@@ -33,9 +53,7 @@ echo"Idoso";
 }
 }
 }
-
 $pessoa->exibirInformacoes();
-
 echo"Dado satualizados com sucesso!";
 }else{
 header("Location:update_form.php");
